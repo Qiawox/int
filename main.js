128c07064870437648eef7657673d0541ef9044d
Qiawox <gv7361299@gmail.com>
2022-05-23 16:28:41 +0400

    Update main.js

--- main.js
+++ main.js
@@ -12,16 +12,39 @@ ufo.ondragstart = drag;
 
 function drag(event){
   event.dataTransfer.setData('id', event.target.id);
-  localStorage.setItem('id', event.target.id);
-  if (localStorage.getItem('id')) {
-    zone1.innerHTML = localStorage.getItem('id');
-  }
+  var rect = ufo.getBoundingClientRect();
+  console.log(rect.top, rect.right, rect.bottom, rect.left);
+  localStorage.setItem('id', JSON.stringify(rect));
 }
 
 zone1.ondrop = drop;
 zone2.ondrop = drop;
 function drop(event){
   let itemId = event.dataTransfer.getData('id');
+  var getik = localStorage.getItem('id');
   console.log(itemId);
+  data = JSON.parse(getik);
+  position = {};
+  position.top = data.top || 0;
+  position.left = data.left || 0;
+  $('#text').css({
+    top : position.top,
+    left : position.left
+  })
+  // $('#text').draggable({
+  //   drag: function(){
+  //     getik = JSON.stringify({
+  //     'top' : $(this).offset().top,
+  //     'left' : $(this).offset().left,
+  //     })
+  //   }
+  // })
   event.target.append(document.getElementById(itemId));
+  // return getik ? JSON.parse(getik) : [];
 }
+
+// zone1.ondrop = handleStop;
+// zone2.ondrop  = handleStop;
+// function handleStop(e, data) {
+  
+// }
