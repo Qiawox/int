7b5097b5c6a9a6743537a15bca3fa0296d10b900
Qiawox <gv7361299@gmail.com>
2022-05-23 17:39:32 +0400

    Update main.js

--- main.js
+++ main.js
@@ -18,19 +18,24 @@ function drag(event){
 }
 
 zone1.ondrop = drop;
-zone2.ondrop = drop;
+zone2.ondrop = drop1;
 function drop(event){
   let itemId = event.dataTransfer.getData('id');
   var getik = localStorage.getItem('id');
   console.log(itemId);
-  data = JSON.parse(getik);
-  position = {};
-  position.top = data.top || 0;
-  position.left = data.left || 0;
-  $('#text').css({
-    top : position.top,
-    left : position.left
-  })
+  document.getElementById('text').style.position = 'absolute';
+  document.getElementById('text').style.color = 'green';
+  document.getElementById('text').style.fontSize = '72px';
+  document.getElementById('text').innerHTML = 'netext - i cant make dragndrop + localstorage';
+  document.getElementById('text').style.fontFamily = '"Macondo', "cursive'";
+  document.getElementById('text').style.paddingTop = '100px';
+  document.getElementById('text').style.transform = 'scale(1.2)';
+  document.getElementById('text').style.transform = 'rotate(-5deg)';
+  document.getElementById('text').style.transform = 'skewX(-50deg)';
+  // document.getElementById('text').style.transform = 'skewY(-10deg)';
+  document.getElementById('text').style.left = '200px';
+  var local_value = getik.replace("/\n/g", "<br />");
+  $('#result').html(local_value);
   // $('#text').draggable({
   //   drag: function(){
   //     getik = JSON.stringify({
@@ -42,7 +47,30 @@ function drop(event){
   event.target.append(document.getElementById(itemId));
   // return getik ? JSON.parse(getik) : [];
 }
-
+function drop1(event){
+  let itemId = event.dataTransfer.getData('id');
+  var getik = localStorage.getItem('id');
+  console.log(itemId);
+  document.getElementById('text').style.position = 'absolute';
+  document.getElementById('text').style.color = 'red';
+  document.getElementById('text').style.fontSize = '72px';
+  document.getElementById('text').innerHTML = 'text - i cant make dragndrop + localstorage';
+  document.getElementById('text').style.fontFamily = '"Macondo', "cursive'";
+  document.getElementById('text').style.paddingTop = '80px';
+  document.getElementById('text').style.left = '200px';
+  var local_value = getik.replace("/\n/g", "<br />");
+  $('#result').html(local_value);
+  // $('#text').draggable({
+  //   drag: function(){
+  //     getik = JSON.stringify({
+  //     'top' : $(this).offset().top,
+  //     'left' : $(this).offset().left,
+  //     })
+  //   }
+  // })
+  event.target.append(document.getElementById(itemId));
+  // return getik ? JSON.parse(getik) : [];
+}
 // zone1.ondrop = handleStop;
 // zone2.ondrop  = handleStop;
 // function handleStop(e, data) {
